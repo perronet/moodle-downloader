9a25bff3b2175231ac924b1cfc9ee8fd32460b56
Marco Perronet <arnad.1997@gmail.com>
2018-10-11 02:39:31 +0200

    Folder download now works, added config file

---
+++ configurazione.sh
@@ -0,0 +1,29 @@
+#Inserisci fra le due parentesi i link dei corsi che vuoi scaricare, uno per ogni riga
+#Occhio: Devi per forza essere iscritto alla pagina del corso!
+#Esempio:
+#corsi=(
+#informatica.i-learn.unito.it/course/view.php?id=1612
+#informatica.i-learn.unito.it/course/view.php?id=1576
+#informatica.i-learn.unito.it/course/view.php?id=1578
+#)
+
+corsi=(
+
+)
+
+#Nomi delle cartelle corrispondenti ai link dei corsi
+#Esempio:
+#cartelle=(
+#Reti 
+#Prog3 
+#IUMTweb
+#)
+
+cartelle=(
+
+)
+
+#Username di moodle
+#Esempio: user='mario.coppo'
+
+user=''
--- moodledownloader.sh
+++ moodledownloader.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-#########CONFIGURAZIONE###########
-#Id corrispondenti ai link dei vari corsi, separati da spazi
-#Puoi trovare l'id del corso alla fine del link della sua pagina su moodle
-#Esempio: corsi=(1565 1608 1612 1576 1622)
-corsi=()
-
-#Nomi delle cartelle corrispondenti agli id dei corsi, separati da spazi
-#Esempio: cartelle=(LPP MFI Reti Prog3 Tweb)
-cartelle=()
-
-#Username di moodle
-#Esempio: user='mario.coppo'
-user=''
-##################################
-
 #Per eventuali bug contattami su telegram @v4lley
 #Disclaimer: Se lo script è lento è colpa della connessione, specialmente se state usando quella dell'uni ;)
 #Disclaimer: Se lo script mette i file in sottocartelle sbagliate generalmente è perchè i prof li hanno caricati nelle cartelle sbagliate
 
-link='informatica.i-learn.unito.it/course/view.php?id='
+source configurazione.sh
 
 if [[ -z $user ]]
 	then
@@ -40,22 +25,64 @@ wget --save-cookies cookies.txt \
 #Download dei file per ogni corso
 for ((i=0;i<${#corsi[@]};i++))
 do
+	#Dowload delle cartelle
+	#wget su link "/mod/folder/", cartelle
+	#folders contiene i link di tutte le cartelle presenti nel corso
+	folders="$(wget --load-cookies cookies.txt \
+	-O - \
+	"${corsi[i]}" \
+	| grep -Po 'http://informatica.i-learn.unito.it/mod/folder/view.php\?id=\d+')"
+
+	#Trova i nomi corretti delle cartelle
+	k=0
+	for a in $folders
+	do
+		#foldernames contiene i nomi corrispondenti alle cartelle
+		foldernames[k]="$(wget --load-cookies cookies.txt \
+		-O - \
+		"$a" \
+		| grep -Po '(?<=<title>).*(?=</title>)' \
+		| cut -d ':' -f 2- \
+		| tr / - \
+		| sed -e 's/^[[:space:]]*//')"
+
+		((k++))
+	done	
+
+	#Trasforma in array
+	mapfile -t folders < <(echo "$folders")
+
+	#Scarica le cartelle coi rispettivi nomi
+	for ((k=0;k<${#folders[@]};k++))
+	do
+		wget --load-cookies cookies.txt \
+		--recursive \
+		--accept-regex 'informatica.i-learn.unito.it/.*/mod_folder/content/.*' \
+		-nH \
+		--cut-dirs=5 \
+		--content-disposition \
+		--no-clobber \
+		-P "./${cartelle[i]}/${foldernames[k]}" \
+		"${folders[k]}"
+	done	
+
+	#Dowload dei file fuori dalle cartelle
 	#wget su link "/mod/resource", generalmente fanno un redirect su un altro link su cui si trova il file
 	#wget su link "/mod_resource/content/", generalmente link diretti al file
-	#wget su link "/mod/folder/", cartelle
 	wget --load-cookies cookies.txt \
 		--recursive \
-		--accept-regex 'informatica.i-learn.unito.it/(mod/resource/.*|mod/folder/.*|.*/mod_resource/content/.*|.*/mod_folder/content/.*)' \
+		--accept-regex 'informatica.i-learn.unito.it/(mod/resource/.*|.*/mod_resource/content/.*)' \
 		--reject-regex 'informatica.i-learn.unito.it/mod/resource/.*&lang=.*' \
 		-nH \
 		--cut-dirs=5 \
 		--content-disposition \
 		--no-clobber \
 		-P "./${cartelle[i]}" \
-		"$link${corsi[i]}"
+		"${corsi[i]}"
 
-	#Copia della pagina html
-	mv "${cartelle[i]}/view.php?id=${corsi[i]}" "${cartelle[i]}/${cartelle[i]}Moodle.html"
+	#Copia della pagina html del corso
+	idcorso="$(echo "${corsi[i]}" | grep -Po 'informatica.i-learn.unito.it/course/view.php\?id=\K\d+')"
+	mv "${cartelle[i]}/view.php?id=$idcorso" "${cartelle[i]}/${cartelle[i]}Moodle.html"
 done
 
 find . -name 'view.php?*' -type f -delete
